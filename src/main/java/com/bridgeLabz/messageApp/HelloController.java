65e7bd4f72e684e70bcee3b981b819b59fb94718
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-24 10:56:14 +0530

    [tarun] Add. path variable and query parameter

--- src/main/java/com/bridgeLabz/messageApp/HelloController.java
+++ src/main/java/com/bridgeLabz/messageApp/HelloController.java
@@ -1,15 +1,17 @@
 package com.bridgeLabz.messageApp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
+@RequestMapping("/hi")
 @RestController()
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello from BridgeLabz";
+    }
+    @PutMapping("/put/{firstName}")
+    public String hello(@PathVariable String firstName,@RequestParam String lastName) {
+        return "First Name: "+firstName + " Last Name: " + lastName;
     }
 }
